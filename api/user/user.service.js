3fb8c82bf86622ba3ba8364f581582035675a4d0
Assaf Fendell <fendell.assaf@gmail.com>
2021-06-06 12:32:39 +0300

    mashoo

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -38,12 +38,6 @@ async function getById(userId) {
         const user = await collection.findOne({ '_id': ObjectId(userId) })
         delete user.password
 
-        user.givenReviews = await reviewService.query({ byUserId: ObjectId(user._id) })
-        user.givenReviews = user.givenReviews.map(review => {
-            delete review.byUser
-            return review
-        })
-
         return user
     } catch (err) {
         logger.error(`while finding user ${userId}`, err)
